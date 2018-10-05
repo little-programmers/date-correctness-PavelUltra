e48fc6f69f335371eb228bb3b30c65336d2b0f7f
PavelUltra <43610301+PavelUltra@users.noreply.github.com>
2018-10-05 19:43:45 +0700

    main.c by Pashok

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 
 // Функция возращает true, если дата корректна, иначе - false.
 // Вы можете объявлять дополнительные функции, главное, чтобы присутствовала эта функция.
-// Вместо точки с запятой, открывайте фигурные скобки и начинайте писать.
-bool date_correctness(int day, int month, int year);
+// Вместо точки с запятой, открывайте фигурные скобки и начинайте писать.bool Opacha()bool Opacha()
+bool date_correctness(int day, int month, int year){ 
 
+ int MonthArray[]= {31,year % 4 == 0 ? (year % 100 == 0 ? (year % 400 == 0 ? 29 : 28) : 29) : 28 ,31,30,31,30,31,31,30,31,30,31};
+	return ((year > 0) && (0 < day && day <= 31) && (0< month && month < 13)) ? day <= MonthArray[month-1]:false;
+}
 
 void test_date_correctness() {
     assert(date_correctness(1,1,2018));
@@ -20,7 +23,7 @@ void test_date_correctness() {
     assert(!date_correctness(1,3,-1));
     assert(!date_correctness(2,13,2001));
     assert(!date_correctness(40,5,777));
-    assert(!date_correctness(29,2,1900));
+    assert(!date_correctness(29,2,1900)); // 1900 - visokosni god, tam dolshno bit 29 dney v fevrale
 
     printf("Тесты прошли успешно!\n");
 }
